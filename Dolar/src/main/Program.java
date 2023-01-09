0be0ac802182f58496d993f3208ebc32dc7e493f
jzhumm <61802687+jzhumm@users.noreply.github.com>
2023-01-09 12:44:26 -0300

    Add files via upload

---
+++ Dolar/src/main/Program.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.CurrencyConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+		System.out.println("What is the dollar price? ");
+		double value = sc.nextDouble();
+			
+		System.out.println("How many dollars will be bought? ");
+		double manyD = sc.nextDouble();
+		
+		
+		
+		double reais = (value * manyD) + ( value * manyD * 6/100);
+		
+		
+		
+		System.out.println("Amount to be paid in reais? " + reais );
+			
+		;
+
+	}
+
+}
---
+++ Dolar/src/util/CurrencyConverter.java
@@ -0,0 +1,15 @@
+package util;
+
+public class CurrencyConverter {
+	
+	
+	
+	
+	public static double calcReais() {
+		
+	 
+		return value * manyD + value * (6/100);
+	
+
+}
+}
